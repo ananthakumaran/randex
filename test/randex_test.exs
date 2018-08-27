6959b867e8c6da63693303aeadbc138e4ee3c1f2
Anantha Kumaran <ananthakumaran@gmail.com>
2018-08-27 18:37:41 +0530

    udate tests

--- test/randex_test.exs
+++ test/randex_test.exs
@@ -1,23 +1,8 @@
 defmodule RandexTest do
   require TestHelper
   import TestHelper
-  require Logger
 
   use ExUnit.Case
-
-  def gen(c) do
-    Logger.info("Regex: " <> inspect(c))
-    regex = Regex.compile!(c)
-
-    ast = Randex.Parser.parse(c)
-    Logger.info("AST: " <> inspect(ast, pretty: true))
-
-    Randex.Generator.gen(ast)
-    |> Enum.take(10)
-    |> Enum.each(fn sample ->
-      assert sample =~ regex
-    end)
-  end
 
   regtest("gen")
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,18 +1,49 @@
 defmodule TestHelper do
+  require Logger
+  import ExUnit.Assertions
+
   def cases do
     File.read!(Path.join(__DIR__, "fixture"))
     |> String.split("\n\n")
     |> Enum.filter(&(!String.starts_with?(&1, "#")))
   end
 
+  def gen(c) do
+    Logger.info("Regex: " <> inspect(c))
+    regex = Regex.compile!(c)
+
+    ast = Randex.Parser.parse(c)
+    Logger.info("AST: " <> inspect(ast, pretty: true))
+
+    Randex.Generator.gen(ast)
+    |> Enum.take(10)
+    |> Enum.each(fn sample ->
+      assert sample =~ regex
+    end)
+  end
+
   defmacro regtest(name) do
     prefix = name <> " "
 
     Enum.with_index(cases())
-    |> Enum.map(fn {c, i} ->
+    |> Enum.chunk_every(50)
+    |> Enum.with_index()
+    |> Enum.map(fn {cases, i} ->
       quote do
-        test unquote(prefix) <> unquote(to_string(i)) do
-          __MODULE__.unquote(Macro.var(String.to_atom(name), __MODULE__))(unquote(c))
+        defmodule unquote(String.to_atom("RandexTest#{i}")) do
+          use ExUnit.Case
+
+          unquote do
+            Enum.map(cases, fn {c, j} ->
+              quote do
+                test unquote(prefix) <> unquote(to_string(j)) do
+                  unquote(__MODULE__).unquote(Macro.var(String.to_atom(name), __MODULE__))(
+                    unquote(c)
+                  )
+                end
+              end
+            end)
+          end
         end
       end
     end)
