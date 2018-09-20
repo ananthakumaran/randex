38dd01b1f1b69d83aecb97e0adf7ecf88ca779e6
Anantha Kumaran <ananthakumaran@gmail.com>
2018-09-20 14:14:53 +0530

    make the generator configurable

--- lib/randex/generator.ex
+++ lib/randex/generator.ex
@@ -3,30 +3,34 @@ defmodule Randex.Generator do
 
   alias Randex.AST
   alias Randex.Utils
-  import Randex.Amb
+
+  defmodule Config do
+    @moduledoc false
+    defstruct mod: Randex.Amb, max_repetition: 100
+  end
 
   defmodule State do
     @moduledoc false
     defstruct group: %{}, stack: []
   end
 
-  def gen(asts) do
-    gen_loop(asts, constant({"", %State{}}))
-    |> map(fn {candidate, _state} -> candidate end)
-    |> to_stream
+  def gen(asts, config \\ %Config{}) do
+    gen_loop(asts, config, config.mod.constant({"", %State{}}))
+    |> config.mod.map(fn {candidate, _state} -> candidate end)
+    |> config.mod.to_stream
   end
 
-  defp do_gen(%AST.Char{value: char, caseless: caseless}) do
+  defp do_gen(%AST.Char{value: char, caseless: caseless}, config) do
     if caseless do
-      member_of(Utils.swap_char_case(char))
+      config.mod.member_of(Utils.swap_char_case(char))
     else
-      constant(char)
+      config.mod.constant(char)
     end
   end
 
-  defp do_gen({:group_end, %AST.Group{name: name, number: n}}) do
+  defp do_gen({:group_end, %AST.Group{name: name, number: n}}, config) do
     fun = fn generator ->
-      map(generator, fn {new_candidate, state} ->
+      config.mod.map(generator, fn {new_candidate, state} ->
         [candidate | rest] = state.stack
         state = %{state | stack: rest}
         group_candidate = String.replace_prefix(new_candidate, candidate, "")
@@ -45,23 +49,23 @@ defmodule Randex.Generator do
     {:cont, fun}
   end
 
-  defp do_gen(%AST.Group{values: asts} = group) do
+  defp do_gen(%AST.Group{values: asts} = group, config) do
     fun = fn generator, rest ->
       generator =
-        map(generator, fn {candidate, state} ->
+        config.mod.map(generator, fn {candidate, state} ->
           state = %{state | stack: [candidate | state.stack]}
           {candidate, state}
         end)
 
-      gen_loop(asts ++ [{:group_end, group}] ++ rest, generator)
+      gen_loop(asts ++ [{:group_end, group}] ++ rest, config, generator)
     end
 
     {:cont_rest, fun}
   end
 
-  defp do_gen(%AST.BackReference{} = backref) do
+  defp do_gen(%AST.BackReference{} = backref, config) do
     fun = fn generator ->
-      bind_filter(
+      config.mod.bind_filter(
         generator,
         fn {candidate, state} ->
           value =
@@ -74,7 +78,7 @@ defmodule Randex.Generator do
             end
 
           if value do
-            {:cont, constant({candidate <> value, state})}
+            {:cont, config.mod.constant({candidate <> value, state})}
           else
             :skip
           end
@@ -85,9 +89,9 @@ defmodule Randex.Generator do
     {:cont, fun}
   end
 
-  defp do_gen(%AST.LookBehind{positive: positive, value: value}) do
+  defp do_gen(%AST.LookBehind{positive: positive, value: value}, config) do
     fun = fn generator ->
-      bind_filter(
+      config.mod.bind_filter(
         generator,
         fn {candidate, state} ->
           value =
@@ -98,7 +102,7 @@ defmodule Randex.Generator do
             end
 
           if value do
-            {:cont, constant({candidate, state})}
+            {:cont, config.mod.constant({candidate, state})}
           else
             :skip
           end
@@ -109,9 +113,11 @@ defmodule Randex.Generator do
     {:cont, fun}
   end
 
-  defp do_gen(%AST.LookAhead{positive: positive, value: value}) do
+  defp do_gen(%AST.LookAhead{positive: positive, value: value}, config) do
     fun = fn generator, rest ->
-      bind_gen(generator, rest, &bind_filter/2, fn candidate, sub_candidate, state ->
+      bind_gen(generator, config, rest, &config.mod.bind_filter/2, fn candidate,
+                                                                      sub_candidate,
+                                                                      state ->
         value =
           if positive do
             sub_candidate =~ value
@@ -120,7 +126,7 @@ defmodule Randex.Generator do
           end
 
         if value do
-          {:cont, constant({candidate <> sub_candidate, state})}
+          {:cont, config.mod.constant({candidate <> sub_candidate, state})}
         else
           :skip
         end
@@ -130,13 +136,13 @@ defmodule Randex.Generator do
     {:cont_rest, fun}
   end
 
-  defp do_gen(%AST.Assertion{value: value, ahead: false}) do
+  defp do_gen(%AST.Assertion{value: value, ahead: false}, config) do
     fun = fn generator ->
-      bind_filter(
+      config.mod.bind_filter(
         generator,
         fn {candidate, state} ->
           if candidate =~ ~r/#{value}\z/ do
-            {:cont, constant({candidate, state})}
+            {:cont, config.mod.constant({candidate, state})}
           else
             :skip
           end
@@ -147,9 +153,11 @@ defmodule Randex.Generator do
     {:cont, fun}
   end
 
-  defp do_gen(%AST.Assertion{value: value, ahead: true}) when value in ["\\b", "\\B"] do
+  defp do_gen(%AST.Assertion{value: value, ahead: true}, config) when value in ["\\b", "\\B"] do
     fun = fn generator, rest ->
-      bind_gen(generator, rest, &bind_filter/2, fn candidate, sub_candidate, state ->
+      bind_gen(generator, config, rest, &config.mod.bind_filter/2, fn candidate,
+                                                                      sub_candidate,
+                                                                      state ->
         match =
           case value do
             "\\b" ->
@@ -162,7 +170,7 @@ defmodule Randex.Generator do
           end
 
         if match do
-          {:cont, constant({candidate <> sub_candidate, state})}
+          {:cont, config.mod.constant({candidate <> sub_candidate, state})}
         else
           :skip
         end
@@ -172,11 +180,13 @@ defmodule Randex.Generator do
     {:cont_rest, fun}
   end
 
-  defp do_gen(%AST.Assertion{value: value, ahead: true}) do
+  defp do_gen(%AST.Assertion{value: value, ahead: true}, config) do
     fun = fn generator, rest ->
-      bind_gen(generator, rest, &bind_filter/2, fn candidate, sub_candidate, state ->
+      bind_gen(generator, config, rest, &config.mod.bind_filter/2, fn candidate,
+                                                                      sub_candidate,
+                                                                      state ->
         if sub_candidate =~ ~r/\A#{value}/ do
-          {:cont, constant({candidate <> sub_candidate, state})}
+          {:cont, config.mod.constant({candidate <> sub_candidate, state})}
         else
           :skip
         end
@@ -186,76 +196,76 @@ defmodule Randex.Generator do
     {:cont_rest, fun}
   end
 
-  defp do_gen(%AST.Class{} = ast) do
-    gen_class(ast)
+  defp do_gen(%AST.Class{} = ast, config) do
+    gen_class(ast, config)
   end
 
-  defp do_gen(%AST.Option{}) do
-    constant("")
+  defp do_gen(%AST.Option{}, config) do
+    config.mod.constant("")
   end
 
-  defp do_gen(%AST.Comment{}) do
-    constant("")
+  defp do_gen(%AST.Comment{}, config) do
+    config.mod.constant("")
   end
 
-  defp do_gen(%AST.Verb{}) do
-    constant("")
+  defp do_gen(%AST.Verb{}, config) do
+    config.mod.constant("")
   end
 
-  defp do_gen(%AST.Lazy{value: value}) do
+  defp do_gen(%AST.Lazy{value: value}, config) do
     fun = fn generator ->
-      gen_loop([value], generator)
+      gen_loop([value], config, generator)
     end
 
     {:cont, fun}
   end
 
-  defp do_gen(%AST.Possesive{value: value}) do
+  defp do_gen(%AST.Possesive{value: value}, config) do
     fun = fn generator ->
-      gen_loop([value], generator)
+      gen_loop([value], config, generator)
     end
 
     {:cont, fun}
   end
 
-  defp do_gen(%AST.Repetition{min: min, max: max, value: ast}) do
+  defp do_gen(%AST.Repetition{min: min, max: max, value: ast}, config) do
     fun = fn generator ->
       max =
         if max == :infinity do
-          min + 100
+          min + config.max_repetition
         else
           max
         end
 
-      integer(min..max)
-      |> bind(fn n ->
-        repeat(generator, n, fn g -> gen_loop([ast], g) end)
+      config.mod.integer(min..max)
+      |> config.mod.bind(fn n ->
+        config.mod.repeat(generator, n, fn g -> gen_loop([ast], config, g) end)
       end)
     end
 
     {:cont, fun}
   end
 
-  defp do_gen(%AST.Or{left: left, right: right}) do
+  defp do_gen(%AST.Or{left: left, right: right}, config) do
     fun = fn generator, rest ->
-      member_of([left, right])
-      |> bind(fn ast ->
-        gen_loop(ast ++ rest, generator)
+      config.mod.member_of([left, right])
+      |> config.mod.bind(fn ast ->
+        gen_loop(ast ++ rest, config, generator)
       end)
     end
 
     {:cont_rest, fun}
   end
 
-  defp do_gen(%AST.Range{first: first, last: last}) do
+  defp do_gen(%AST.Range{first: first, last: last}, config) do
     fun = fn generator ->
-      bind(gen([first]), fn <<first::utf8>> ->
-        bind(gen([last]), fn <<last::utf8>> ->
-          integer(first..last)
-          |> map(&<<&1::utf8>>)
-          |> bind(fn char ->
-            bind(generator, fn {candidate, state} ->
-              constant({candidate <> char, state})
+      config.mod.bind(gen([first], config), fn <<first::utf8>> ->
+        config.mod.bind(gen([last], config), fn <<last::utf8>> ->
+          config.mod.integer(first..last)
+          |> config.mod.map(&<<&1::utf8>>)
+          |> config.mod.bind(fn char ->
+            config.mod.bind(generator, fn {candidate, state} ->
+              config.mod.constant({candidate <> char, state})
             end)
           end)
         end)
@@ -265,11 +275,11 @@ defmodule Randex.Generator do
     {:cont, fun}
   end
 
-  defp do_gen(%AST.Dot{}) do
-    string()
+  defp do_gen(%AST.Dot{}, config) do
+    config.mod.string()
   end
 
-  defp gen_class(%AST.Class{values: asts, negate: negate, caseless: caseless}) do
+  defp gen_class(%AST.Class{values: asts, negate: negate, caseless: caseless}, config) do
     Enum.map(asts, fn
       %AST.Char{value: <<char::integer>>} ->
         char..char
@@ -291,42 +301,42 @@ defmodule Randex.Generator do
     |> Utils.non_overlapping([])
     |> Utils.negate_range(negate)
     |> Enum.map(fn range ->
-      integer(range)
-      |> map(&Utils.integer_to_string(&1))
+      config.mod.integer(range)
+      |> config.mod.map(&Utils.integer_to_string(&1))
     end)
-    |> one_of()
+    |> config.mod.one_of()
   end
 
-  defp gen_loop([], generator), do: generator
+  defp gen_loop([], _config, generator), do: generator
 
-  defp gen_loop([ast | rest], generator) do
-    case do_gen(ast) do
+  defp gen_loop([ast | rest], config, generator) do
+    case do_gen(ast, config) do
       {:cont, fun} ->
-        gen_loop(rest, fun.(generator))
+        gen_loop(rest, config, fun.(generator))
 
       {:cont_rest, fun} ->
         fun.(generator, rest)
 
       current ->
         generator =
-          bind(generator, fn {old, state} ->
-            bind(current, fn new ->
-              constant({old <> new, state})
+          config.mod.bind(generator, fn {old, state} ->
+            config.mod.bind(current, fn new ->
+              config.mod.constant({old <> new, state})
             end)
           end)
 
-        gen_loop(rest, generator)
+        gen_loop(rest, config, generator)
     end
   end
 
-  defp bind_gen(generator, sub, combiner, callback) do
+  defp bind_gen(generator, config, sub, combiner, callback) do
     generator =
-      map(generator, fn {candidate, state} ->
+      config.mod.map(generator, fn {candidate, state} ->
         state = %{state | stack: [candidate | state.stack]}
         {candidate, state}
       end)
 
-    gen_loop(sub, generator)
+    gen_loop(sub, config, generator)
     |> combiner.(fn {new_candidate, state} ->
       [candidate | rest] = state.stack
       state = %{state | stack: rest}
